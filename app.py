1951bd8073a953f347125051d915820dfa878f8c
arjmarty <annrose0227@gmail.com>
2025-05-07 23:05:06 +0800

    adding basic chat input via streamlit

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-first_message = st.chat_input("Hi! How may I help you?")
+first_message = st.chat_input("Hi! How may I help you?", accept_file=True, file_type=None)
+
+if first_message and first_message.text:
+    st.markdown(first_message.text)

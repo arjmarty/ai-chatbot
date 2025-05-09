aef3c629c6097d2d80f746b3b99e5568df764b75
arjmarty <annrose0227@gmail.com>
2025-05-09 18:21:21 +0800

    updated chatbot features

--- app.py
+++ app.py
@@ -2,5 +2,16 @@ import streamlit as st
 
 first_message = st.chat_input("Hi! How may I help you?", accept_file=True, file_type=None)
 
+intro_msg = "Hello and Welcome to COARE!"
+
+if st.button("Press this button"):
+    st.write_stream(intro_msg)
+
 if first_message and first_message.text:
     st.markdown(first_message.text)
+
+if first_message and first_message["files"]:
+    st.image(first_message["files"][0])
+
+with st.chat_message("user"):
+    st.write("Hello stranger!")

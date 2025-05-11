a9cfb89bb6439df7fa6adfdd8e1402ab069150f4
arjmarty <annrose0227@gmail.com>
2025-05-11 22:46:32 +0800

    adding openai

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import openai
 
 first_message = st.chat_input("Hi! How may I help you?", accept_file=True, file_type=None)
 
 intro_msg = "Hello and Welcome to COARE!"
 
 if st.button("Press this button"):
-    st.write_stream(intro_msg)
+    st.write(intro_msg)
 
 if first_message and first_message.text:
     st.markdown(first_message.text)

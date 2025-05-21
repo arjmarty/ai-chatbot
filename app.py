8cf561dcbf97514b3c8a59532a093fbcd7c419eb
arjmarty <annrose0227@gmail.com>
2025-05-21 23:45:14 +0800

    openai initialization scripts

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import os
-import openai
+from openai import OpenAI
 
-openai.api_key = os.environ['OPENAI_API_KEY']
+
+# Set OpenAI API key
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+#set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+#Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
 first_message = st.chat_input("Hi! How may I help you?", accept_file=True, file_type=None)
 
 intro_msg = "Hello and Welcome to COARE!"
